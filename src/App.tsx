ed094f63d32c9dbcc22f94093a2714749ee66ef0
Ravi Putta <ravikanthputta@gmail.com>
2024-12-29 12:50:18 -0800

    add

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold">Welcome to React</h1>
+    </div>
+  )
+}
+
+export default App
